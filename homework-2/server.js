f4531eaabb03c8b7bb1a8b13cc5be2b4d119c276
xedeen <kutuzov_vs>
2020-04-01 10:53:12 +0700

    homework 1.2

--- homework-2/server.js
+++ homework-2/server.js
@@ -4,15 +4,15 @@ const interval = process.env.INTERVAL || 2000;
 const duration = process.env.DURATION || 10000;
 
 http.createServer(async (req, res) => {
-  const currentTime = () => new Date().toUTCString();
+    const currentTime = () => new Date().toUTCString();
 
-  console.log(currentTime());
+    console.log(currentTime());
 
-  const timer = setInterval(currentTime(), interval);
+    const timer = setInterval(currentTime(), interval);
 
-  setTimeout(() => {
-    clearInterval(timer);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(currentTime());
-  }, duration);
+    setTimeout(() => {        clearInterval(timer);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(currentTime())
+    }, duration)
+
 }).listen(8080);
